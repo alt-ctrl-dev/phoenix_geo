70d301301544c2b8bccf9eab09f301be9e56c2dd
Reuben Coutinho <reubencoutinho@Reubens-Mac-Studio.local>
2024-11-09 13:01:24 +0800

    update styling

--- mix.exs
+++ mix.exs
@@ -63,7 +63,8 @@ defmodule PhoenixGeo.MixProject do
   # See the documentation for `Mix` for more info on aliases.
   defp aliases do
     [
-      setup: ["deps.get", "ecto.setup"],
+      setup: ["deps.get", "assets.setup", "ecto.setup"],
+      "assets.setup": ["tailwind.install", "esbuild.install"],
       "ecto.setup": ["ecto.create", "ecto.migrate", "run priv/repo/seeds.exs"],
       "ecto.reset": ["ecto.drop", "ecto.setup"],
       test: ["ecto.create --quiet", "ecto.migrate --quiet", "test"],
